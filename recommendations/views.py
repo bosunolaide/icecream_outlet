d9cea54c811c76399dc9f5b0b7f46b4379e6b5aa
Abiola Olatunbosun <olaide_abiola87@yahoo.com>
2025-10-30 11:17:33 +0000

    updated recommendations/views.py

--- recommendations/views.py
+++ recommendations/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from flavours.serializers import FlavourSerializer
+from .ml.recommender import FlavourRecommender
 
-# Create your views here.
+class RecommendFlavoursView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        recommender = FlavourRecommender()
+        recommendations = recommender.recommend_for_user(request.user.id)
+        serializer = FlavourSerializer(recommendations, many=True)
+        return Response(serializer.data)
