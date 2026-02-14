291a3e18df79fecd779dde89409c76f3a324216f
Abiola Olatunbosun <olaide_abiola87@yahoo.com>
2026-02-14 11:07:34 +0000

    updated settings.py

--- icecream_outlet/settings.py
+++ icecream_outlet/settings.py
@@ -47,7 +47,7 @@ ROOT_URLCONF = "icecream_outlet.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
